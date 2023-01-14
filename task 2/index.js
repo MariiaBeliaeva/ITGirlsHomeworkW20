a35acd30f9d25ecaad93c3074d152c6f65a7059e
Sirius Black <mariia.beliaev@gmail.com>
2023-01-14 23:46:51 +0300

    adde task 2

---
+++ task 2/index.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function (e) {
+    fetch('https://api.nasa.gov/planetary/apod?api_key=jg4kSp5vTeI5dPMsPdSBYzn7g0hIVocOuXl5cQt7')
+        .then(response => response.json())
+        .then(data => {
+            const title = document.querySelector('.title');
+            title.innerHTML = data.title;
+            const image = document.querySelector('.image');
+            image.src = data.url;
+            const info = document.querySelector('.info');
+            info.innerHTML = data.explanation;
+        })
+        .catch(err => console.log(err));
+})
